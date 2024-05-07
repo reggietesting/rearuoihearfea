b4160f710562eb23f78c4cb8ef1c171522857d8b
reggietesting <164204139+reggietesting@users.noreply.github.com>
2024-05-07 09:46:03 -0500

    Add files via upload

--- runjsabc.js
+++ runjsabc.js
@@ -1,4 +1,18 @@
-const abc = await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
-const dec = JSON.parse(abc);
+const urlstr = window.location.search
+const queryparse = new URLSearchParams(urlstr)
 
-console.log(dec)
+if (queryparse.get("customurl")) {
+    fetch("https://"+queryparse.get("customurl"))
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
